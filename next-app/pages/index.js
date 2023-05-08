d43cf3bd4fbcd7683f3b5cec9d250665f02dcd0b
Ahmad Hafik Raimy-RFJB37 <hafik.raimy@motorolasolutions.com>
2023-05-09 00:27:00 +0800

    add tailwindcss and edit styling

--- next-app/pages/index.js
+++ next-app/pages/index.js
@@ -1,20 +1,20 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import React, { useState, useEffect, useRef } from "react";
-import Web3Modal from "web3modal";
-import { BigNumber, providers, utils } from "ethers";
-import { addLiquidity, calculateCD } from "../utils/addLiquidity";
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import React, { useState, useEffect, useRef } from 'react';
+import Web3Modal from 'web3modal';
+import { BigNumber, providers, utils } from 'ethers';
+import { addLiquidity, calculateCD } from '../utils/addLiquidity';
 import {
   getCDTokensBalance,
   getEtherBalance,
   getLPTokensBalance,
   getReserveOfCDTokens,
-} from "../utils/getAmounts";
+} from '../utils/getAmounts';
 import {
   getTokensAfterRemove,
   removeLiquidity,
-} from "../utils/removeLiquidity";
-import { swapTokens, getAmountOfTokensReceivedFromSwap } from "../utils/swap";
+} from '../utils/removeLiquidity';
+import { swapTokens, getAmountOfTokensReceivedFromSwap } from '../utils/swap';
 
 export default function Home() {
   const [loading, setLoading] = useState(false);
@@ -33,8 +33,8 @@ export default function Home() {
   const [addCDTokens, setAddCDTokens] = useState(zero);
   const [removeEther, setRemoveEther] = useState(zero);
   const [removeCD, setRemoveCD] = useState(zero);
-  const [removeLPTokens, setRemoveLPTokens] = useState("0");
-  const [swapAmount, setSwapAmount] = useState("");
+  const [removeLPTokens, setRemoveLPTokens] = useState('0');
+  const [swapAmount, setSwapAmount] = useState('');
   const [tokensToBeReceivedAfterSwap, setTokensToBeReceivedAfterSwap] =
     useState(zero);
 
@@ -81,12 +81,12 @@ export default function Home() {
         setLoading(false);
         // get all updated amounts after the swap
         await getAmounts();
-        setSwapAmount("");
+        setSwapAmount('');
       }
     } catch (error) {
       console.error(error);
       setLoading(false);
-      setSwapAmount("");
+      setSwapAmount('');
     }
   };
 
@@ -178,8 +178,8 @@ export default function Home() {
 
     const { chainId } = await web3Provider.getNetwork();
     if (chainId !== 5) {
-      window.alert("Change the network to Goerli");
-      throw new Error("Change the network to Goerli");
+      window.alert('Change the network to Goerli');
+      throw new Error('Change the network to Goerli');
     }
 
     if (needSigner) {
@@ -202,7 +202,7 @@ export default function Home() {
     if (!walletConnected) {
       // if wallet is not connected, create a new instance of web3modal and connect the metamask wallet
       web3ModalRef.current = new Web3Modal({
-        network: "goerli",
+        network: 'goerli',
         providerOptions: {},
         disableInjectedProvider: false,
       });
@@ -226,9 +226,9 @@ export default function Home() {
 
     if (liquidityTab) {
       return (
-        <>
+        <div>
           <div>
-            <div className={styles.description}>
+            <div className='font-poppins font-lg text-black font-bold'>
               You have:
               <br />
               {utils.formatEther(cdBalance)} Crypto Dev Tokens
@@ -241,17 +241,17 @@ export default function Home() {
           {utils.parseEther(reservedCD.toString()).eq(zero) ? (
             <div>
               <input
-                type="number"
-                placeholder="Amount of Ether"
-                onChange={(e) => setAddEther(e.target.value || "0")}
+                type='number'
+                placeholder='Amount of Ether'
+                onChange={(e) => setAddEther(e.target.value || '0')}
                 className={styles.input}
               />
               <input
-                type="number"
-                placeholder="Amount of CryptoDev Tokens"
+                type='number'
+                placeholder='Amount of CryptoDev Tokens'
                 onChange={(e) =>
                   setAddCDTokens(
-                    BigNumber.from(utils.parseEther(e.target.value || "0"))
+                    BigNumber.from(utils.parseEther(e.target.value || '0'))
                   )
                 }
                 className={styles.input}
@@ -263,12 +263,12 @@ export default function Home() {
           ) : (
             <div>
               <input
-                type="number"
-                placeholder="Amount of Ether"
+                type='number'
+                placeholder='Amount of Ether'
                 onChange={async (e) => {
-                  setAddEther(e.target.value || "0");
+                  setAddEther(e.target.value || '0');
                   const _addCDTokens = await calculateCD(
-                    e.target.value || "0",
+                    e.target.value || '0',
                     etherBalanceContract,
                     reservedCD
                   );
@@ -288,11 +288,11 @@ export default function Home() {
           )}
           <div>
             <input
-              type="number"
-              placeholder="Amount of LP Tokens"
+              type='number'
+              placeholder='Amount of LP Tokens'
               onChange={async (e) => {
-                setRemoveLPTokens(e.target.value || "0");
-                await _getTokensAfterRemove(e.target.value || "0");
+                setRemoveLPTokens(e.target.value || '0');
+                await _getTokensAfterRemove(e.target.value || '0');
               }}
               className={styles.input}
             />
@@ -305,34 +305,34 @@ export default function Home() {
               Remove
             </button>
           </div>
-        </>
+        </div>
       );
     } else {
       return (
         <div>
           <input
-            type="number"
-            placeholder="Amount"
+            type='number'
+            placeholder='Amount'
             onChange={async (e) => {
-              setSwapAmount(e.target.value || "");
+              setSwapAmount(e.target.value || '');
               // calculate the amount of tokens user would be receive after the swap
-              await _getAmountOfTokensReceivedFromSwap(e.target.value || "0");
+              await _getAmountOfTokensReceivedFromSwap(e.target.value || '0');
             }}
             className={styles.input}
             value={swapAmount}
           />
           <select
             className={styles.select}
-            name="dropdown"
-            id="dropdown"
+            name='dropdown'
+            id='dropdown'
             onChange={async (e) => {
               setEthSelected(!ethSelected);
               await _getAmountOfTokensReceivedFromSwap(0);
-              setSwapAmount("");
+              setSwapAmount('');
             }}
           >
-            <option value="eth">Ethereum</option>
-            <option value="cryptoDevToken">Crypto Dev Token</option>
+            <option value='eth'>Ethereum</option>
+            <option value='cryptoDevToken'>Crypto Dev Token</option>
           </select>
           <br />
           <div className={styles.inputDiv}>
@@ -355,41 +355,56 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Crypto Devs</title>
-        <meta name="description" content="DeFi-Exchange" />
-        <link rel="icon" href="/favicon.ico" />
+        <title>Decentralized Exchange</title>
+        <meta name='description' content='DeFi-Exchange' />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
-      <div className={styles.main}>
-        <div>
-          <h1 className={styles.title}>Welcome to Crypto Devs Exchange!</h1>
-          <div className={styles.description}>
-            Exchange Ethereum &#60;&#62; Crypto Dev Tokens
+      <div className='flex justify-center min-h-screen sm:px-16 px-6'>
+        <div className='flex justify-between items-center flex-col max-w-[1280px] w-full'>
+          <header className='flex flex-row justify-between items-center w-full sm:pt-5 pt-6'>
+            <img
+              src='/uniswapLogo.png'
+              alt='uniswap logo'
+              className='w-16 h-16 object-contain'
+            />
+          
+          </header>
+          <div className='flex-1 flex justify-start items-center flex-col w-full mt-5'>
+            <h1 className='text-black font-poppins font-black text-5xl tracking-wide'>
+              Uniswap 1.0
+            </h1>
+            <p className='text-black font-poppins font-medium mt-3 text-base'>
+              Provide liquidity and get rewarded &#60;&#62; Exchange tokens in seconds 
+            </p>
+
+            <div className='mt-5 w-full flex justify-center'>
+              <div className='relative md:max-w-[700px] md:min-w-[500px] min-w-full max-w-full gradient-border p-[2px] rounded-3xl'>
+                <div>
+                  <div className='flex justify-center mb-3'>
+                    <button
+                      className='bg-gradient-to-r from-cyan-500 to-teal-500 hover:from-pink-500 hover:to-yellow-500 text-white px-6 py-2 rounded-md cursor-pointer'
+                      onClick={() => {
+                        setLiquidityTab(true);
+                      }}
+                    >
+                      Liquidity
+                    </button>
+                    <button
+                      className='bg-gradient-to-r from-cyan-500 to-teal-500  hover:from-red-600 hover:to-blue-500 text-white ml-2 px-8 py-2 rounded-md cursor-pointer'
+                      onClick={() => setLiquidityTab(false)}
+                    >
+                      Swap
+                    </button>
+                  </div>
+                  <div>
+                    {renderButton()}
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div>
-            <button
-              className={styles.button}
-              onClick={() => {
-                setLiquidityTab(true);
-              }}
-            >
-              Liquidity
-            </button>
-            <button
-              className={styles.button}
-              onClick={() => setLiquidityTab(false)}
-            >
-              Swap
-            </button>
-          </div>
-          {renderButton()}
-        </div>
-        <div>
-          <img className={styles.image} src="./cryptodev.svg" />
         </div>
       </div>
-      <footer className={styles.footer}>
-        Made with &#10084; by Ahmad Hafik Raimy
-      </footer>
     </div>
   );
 }
